4fd7e69e1f571fcf1a4624be639d4e58c8594318
MUHAMMED SHIBLI K <122768057+mshibili@users.noreply.github.com>
2023-06-10 20:34:47 +0530

    Leak sensor trigger pulse length 1Sec

--- libraries/AP_LeakDetector/AP_LeakDetector_Digital.cpp
+++ libraries/AP_LeakDetector/AP_LeakDetector_Digital.cpp
@@ -14,9 +14,10 @@ void AP_LeakDetector_Digital::read()
         state.sensorPulse= hal.gpio->read(leak_detector._pin[state.instance]) != leak_detector._default_reading[state.instance];
         state.lastPulse = AP_HAL::millis();
         // eg. lastPulse = 12:01:00 (timercount)
+        state.status = AP_HAL::millis() > state.lastPulse + LEAK_SENSOR_DELAY;
+        //  status true only when  12:01:01  
     } else {
         state.status = false;
     }
-    state.status = AP_HAL::millis() > state.lastPulse + LEAK_SENSOR_DELAY;
-    //  status true only when  12:01:01    
+  
 }
